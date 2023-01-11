99578f3658d3dfeb78a95502d69ef0fb7eab962f
Mashopy <eliasgheeraert@gmail.com>
2023-01-11 19:47:59 +0100

    sm8250-common: Add nosdcard to product charateristics
    Their is no Realme devices powered by SM8250 platform who can use a microSD card
    
    Change-Id: Iea8fd67fac3619b0ed77abd0cf794c105142bc00

--- common.mk
+++ common.mk
@@ -144,6 +144,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Charateristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
